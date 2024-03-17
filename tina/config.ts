b16480073c175db70241e98ea5f54deccdde7041
skay138 <nine1352@gmail.com>
2024-03-17 16:27:54 +0900

    working media..

--- tina/config.ts
+++ tina/config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "",
+      mediaRoot: "content/post",
+      publicFolder: "content/post",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
